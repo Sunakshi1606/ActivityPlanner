b5ba2a49ccc3d1caf9c64580520835bbf37b85e9
Sunakshi1606 <sunakshisharma1606@gmail.com>
2025-03-25 14:40:04 +0000

    Updating the URL

--- app/_serverscripts/weatherforecast.ts
+++ app/_serverscripts/weatherforecast.ts
@@ -15,7 +15,7 @@ import { ActivityScore, SearchedLocation, WeatherForecast } from "@/lib/types";
 
 export async function getLocationData(city: string): Promise<SearchedLocation> { 
   const geoResponse = await fetch(
-    `https://localhost:7004/api/GeoCode/geo-code/`+encodeURIComponent(city)    
+    `https://localhost:7004/api/GeoCode/coordinates/`+encodeURIComponent(city)    
   );
   const geoData = await geoResponse.json();    
   if (!geoData) {
