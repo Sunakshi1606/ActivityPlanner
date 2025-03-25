cc699bc7c32e9f0a661a6b274f0c7897465d3599
Sunakshi1606 <sunakshisharma1606@gmail.com>
2025-03-25 14:42:51 +0000

    Removing the unnecessary code

--- app/_serverscripts/weatherforecast.ts
+++ app/_serverscripts/weatherforecast.ts
@@ -1,18 +1,5 @@
 import { ActivityScore, SearchedLocation, WeatherForecast } from "@/lib/types";
 
-// export async function getLocationData(previousState:any, formData:any): Promise<SearchedLocation> { 
-//     let city = formData.get('city')
-//     const geoResponse = await fetch(
-//       `https://localhost:7004/api/GeoCode/geo-code/`+encodeURIComponent(city)    
-//     );
-//     const geoData = await geoResponse.json();    
-//     if (!geoData) {
-//       throw new Error('City not found');
-//     }
-//     return geoData;
-//   }
-  
-
 export async function getLocationData(city: string): Promise<SearchedLocation> { 
   const geoResponse = await fetch(
     `https://localhost:7004/api/GeoCode/coordinates/`+encodeURIComponent(city)    
